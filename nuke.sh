4c2f2968c0b368bbf99708ed589249ad3da033ba
David Arayan <david.arayan12@gmail.com>
2019-06-29 11:13:33 +1000

    - First version to compile and launch in DockerHub

--- nuke.sh
+++ nuke.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Nukes all built docker images
-docker rm -v cardano-node_cardano-node_1
-docker rmi cardano-node_cardano-node:latest --force
+docker rm -v cardano-node_jormungandr_1
+docker rmi cardano-node_jormungandr:latest --force
